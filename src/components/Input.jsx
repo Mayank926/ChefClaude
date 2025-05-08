16363bdf14153b1877c598c48cfb7074da6f6780
Mayank Madhav <mayank.madhav@hotmail.com>
2025-05-08 20:10:26 +0530

    layout complete

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,24 +1,39 @@
 import './Input.css';
 
+let ingredientListItems = [];
+const handleClick = () => {
+  console.log('Button clicked!'); 
+}
+
+const handleMouseOver = () => {
+  console.log('Mouse over!');
+}
+
+const formSubmitted = (event) => {
+  event.preventDefault(); // Prevent the default form submission behavior
+  const formData = new FormData(event.currentTarget);
+  const newIngredient = formData.get('ingredient'); // Get the value of the input field
+  ingredientListItems.push(<li>{newIngredient}</li>); // Add the new ingredient to the list
+  console.log('Form submitted!');
+  console.log(ingredientListItems)
+}
+
 const Input = () => {
   return (
     <div className="input-div">
-      <form className="ingredients-form">
+      <form className="ingredients-form" onSubmit={formSubmitted}>
         <input
           type="text"
-          placeholder="e.g. Oregano"
+          placeholder="e.g. oregano"
           aria-label="Provide igredients"
+          name="ingredient"
         ></input>
-        <button>Add Ingredient</button>
+        <button onClick={handleClick} onMouseOver={handleMouseOver}>Add ingredient</button>
       </form>
       <div className="IngredientsDisplay">
         <h1>Ingredient on hand</h1>
         <ul className="ingredients-list">
-          <li>Oregano</li>
-          <li>Tomato</li>
-          <li>Onion</li>
-          <li>Garlic</li>
-          <li>Olive Oil</li>
+          {ingredientListItems}
         </ul>
       </div>
       <form className="recipe-form">
